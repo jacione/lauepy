bd014007fa506374d62fbd17549fe3730c9bb72d
Nick Porter <jacioneportier@gmail.com>
2022-01-05 15:12:17 -0700

    Adds a step to ensure that all the required fields are provided in the configuration file.

--- lauepy/laue/utils.py
+++ lauepy/laue/utils.py
@@ -1,14 +1,28 @@
-import yaml
+import shutil
 from pathlib import Path
+
+import yaml
+
+# Get the required keys from the default config file
+example = Path(__file__).parents[1] / 'config_example/config.yml'
+with open(example, 'r') as f:
+    REQUIRED = yaml.safe_load(f)
 
 
 def read_config(yml_file):
+    # Load the configuration file
     with open(yml_file, 'r') as f:
         config = yaml.safe_load(f)
-    working_dir = Path(config['working_dir'])
+
+    # Check the config to make sure it has all the right fields
+    if missing_keys := REQUIRED.keys() - config.keys():
+        raise KeyError(f'The following required fields were not found in the config file: {missing_keys}')
+
+    # Create the working directory if needed
     id_dir = Path(f"{config['working_dir']}/{config['working_id']}")
     if not id_dir.exists():
         id_dir.mkdir(parents=True)
+
     return config
 
 
