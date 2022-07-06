9c510e3a1a03d710688ac231bf2509d5251665b4
cxduser <jacioneportier@gmail.com>
2022-07-06 16:01:13 -0500

    Removed unnecessary plotting

--- lauepy/laue/overlay_peaks.py
+++ lauepy/laue/overlay_peaks.py
@@ -16,16 +16,5 @@ def overlay(config, pattern, grain):
                               f"{config['exp_id']}{config['spec_seq']}_S{config['scan']:04}_{frame:05}.tif")
     clean_img = tifffile.imread(f"{config['working_dir']}/clean_images/img_{frame:05}.tiff")
 
-    if config['show_plots']:
-        plt.figure(figsize=[16, 10])
-        plt.subplot(1, 2, 1)
-        plt.imshow(raw_img, vmin=0, vmax=config['grain_threshold'], origin='upper')
-        plt.scatter(xs, ys, s=81, facecolors='none', edgecolors='r')
-        plt.subplot(1, 2, 2)
-        plt.title('red_img_scan' + '_%05d' % frame)
-        plt.imshow(clean_img, vmin=0, vmax=1, origin='upper')
-        plt.scatter(xs, ys, s=81, facecolors='none', edgecolors='r')
-        plt.show()
-
     tifffile.imsave(f"{config['working_dir']}/grains/{grain}.tiff", raw_img.astype(np.float32))
     return
